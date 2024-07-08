047c89fdbb3b68525e32186b93f95b61a296a2bb
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-07-09 00:58:33 +0800

    handle register url api

--- sharebnb_backend/sharebnb_backend/settings.py
+++ sharebnb_backend/sharebnb_backend/settings.py
@@ -40,10 +40,10 @@ ACCOUNT_EMAIL_VERIFICATION = None
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated"
+        "rest_framework.permissions.IsAuthenticated",
     )
 }
 
