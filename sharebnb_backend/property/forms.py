e9e5967b6317aa392448216bb01a03e1a5ac239b
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-07-16 00:20:00 +0800

    added create propery endpoint

---
+++ sharebnb_backend/property/forms.py
@@ -0,0 +1,18 @@
+from .models import Property
+from django.forms import ModelForm
+
+class PropertyForm(ModelForm):
+    class Meta:
+        model = Property
+        fields = (
+            "name",
+            "description",
+            "price_per_night",
+            "bedrooms",
+            "bathrooms",
+            "guests",
+            "country",
+            "country_code",
+            "category",
+            "image"
+        )
